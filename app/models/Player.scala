5bfad99f68432e6f2ef05537ca5f4fc0dab3bb29
Catarina Canto <catarinaccanto@gmail.com>
2019-12-23 14:56:19 +0000

    added bootstrap

--- app/models/Player.scala
+++ app/models/Player.scala
@@ -4,7 +4,6 @@ case class Player(name: String) extends Ordered[Player] {
   val pointsDescription = Array(Score.love.toString, Score.fifteen.toString, Score.thirty.toString, Score.forty.toString)
   var score = 0
   var win: String = name + " wins ball"
-  var numberOfWins = 0
   def scoreDescription: String = pointsDescription(score)
   def compare(that: Player): Int = this.score - that.score
   def winBall(): String = {
